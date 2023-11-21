69a2c36f6508a767a6db75a1c45ae81000a36bfa
Pierre Gauthier <pigau@smile.fr>
2023-11-21 11:36:57 +0100

    Add gally plugin

--- src/Entity/Channel/Channel.php
+++ src/Entity/Channel/Channel.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace App\Entity\Channel;
 
 use Doctrine\ORM\Mapping as ORM;
+use Gally\SyliusPlugin\Model\GallyChannelInterface;
+use Gally\SyliusPlugin\Model\GallyChannelTrait;
 use Sylius\Component\Core\Model\Channel as BaseChannel;
 
 /**
@@ -13,6 +15,7 @@ use Sylius\Component\Core\Model\Channel as BaseChannel;
  */
 #[ORM\Entity]
 #[ORM\Table(name: 'sylius_channel')]
-class Channel extends BaseChannel
+class Channel extends BaseChannel implements GallyChannelInterface
 {
+    use GallyChannelTrait;
 }
